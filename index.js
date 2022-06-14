961e9667c8bf3f9cf1db1df5b152ed3c455618e5
arturoalmiron <arturoalmiron@hotmail.com>
2022-06-14 01:53:00 -0400

    The mix

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const options = {
 	}
 };
 
-fetch('https://v2.jokeapi.dev/joke/Dark?type=twopart', options)
+fetch('https://v2.jokeapi.dev/joke/Dark?type=twopart&amount=2', options)
 	.then(response => response.json())
 	.then(response => {
         console.log(response);
-        document.getElementById('setup').innerHTML = response.setup;
-        document.getElementById('delivery').innerHTML = response.delivery;
-        console.log(response.setup);
-        console.log(response.delivery);
+        document.getElementById('setup').innerHTML = response.jokes[0].setup;
+        document.getElementById('delivery').innerHTML = response.jokes[1].delivery;
+        console.log(response.jokes[0].setup);
+        console.log(response.jokes[1].delivery);
     })
     
 	.catch(err => console.error(err));
