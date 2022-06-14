a74c765a9d2442c6dd3d4cd92dd0dc5934c7f836
arturoalmiron <arturoalmiron@hotmail.com>
2022-06-14 03:16:48 -0400

    Custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}",
+  './index.html',
+],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'comicn': 'Comic Neue'
+      },
+    },
   },
   plugins: [],
 }
